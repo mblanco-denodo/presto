63bf07f6f8ef3391697dddf04290311200c8fa8e
Miguel Blanco <mblanco@denodo.com>
2025-06-04 16:11:35 +0000

    Basic connector classes, does not compile yet WIP

---
+++ presto-native-execution/presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConfig.cpp
@@ -0,0 +1,35 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConfig.h"
+
+namespace facebook::presto {
+  std::optional<std::string> DenodoArrowFlightConfig::connectionUsername()
+      const {
+    return std::optional<std::string>(
+      m_config->get<std::string>(keyConnectionUsername));
+  }
+
+  std::optional<std::string> DenodoArrowFlightConfig::connectionPassword()
+      const {
+    return std::optional<std::string>(
+      m_config->get<std::string>(keyConnectionPassword));
+  }
+
+  std::optional<std::string> DenodoArrowFlightConfig::connectionUserAgent()
+      const {
+    return std::optional<std::string>(
+      m_config->get<std::string>(keyConnectionUserAgent));
+  }
+} // namespace facebook::presto
---
+++ presto-native-execution/presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConnector.h
@@ -0,0 +1,48 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "presto_cpp/main/connectors/arrow_flight/ArrowFlightConnector.h"
+#include "presto_cpp/main/connectors/arrow_flight/auth/Authenticator.h"
+#include "presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConfig.h"
+#include "velox/connectors/Connector.h"
+
+namespace facebook::presto {
+class DenodoArrowFlightConnector final : public ArrowFlightConnector {
+public:
+  explicit DenodoArrowFlightConnector(
+     const std::string& id,
+     std::shared_ptr<const velox::config::ConfigBase> config,
+     const char* authenticatorName = nullptr)
+     : ArrowFlightConnector(id, config, authenticatorName),
+       flightConfig_(std::make_shared<ArrowFlightConfig>(config)),
+       clientOpts_(initClientOpts(flightConfig_)),
+       authenticator_(getAuthenticatorFactory(
+                          authenticatorName
+                              ? authenticatorName
+                              : flightConfig_->authenticatorName())
+                          ->newAuthenticator(config))
+  {
+    // constructor code: none for now
+  }
+
+private:
+  static std::shared_ptr<arrow::flight::FlightClientOptions> initClientOpts(
+      const std::shared_ptr<ArrowFlightConfig>& config);
+
+  const std::shared_ptr<ArrowFlightConfig> flightConfig_;
+  const std::shared_ptr<arrow::flight::FlightClientOptions> clientOpts_;
+  const std::shared_ptr<Authenticator> authenticator_;
+};
+} // namespace facebook::presto
