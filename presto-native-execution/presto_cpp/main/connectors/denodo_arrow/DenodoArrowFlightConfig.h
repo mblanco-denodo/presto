488810e2c15ed1c6676a09708d1333d07a4f7f87
Miguel Blanco <mblanco@denodo.com>
2025-06-04 16:11:35 +0000

    Basic connector classes, does not compile yet WIP

---
+++ presto-native-execution/presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConfig.h
@@ -0,0 +1,38 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "presto_cpp/main/connectors/arrow_flight/ArrowFlightConfig.h"
+
+namespace facebook::presto {
+
+class DenodoArrowFlightConfig final : public ArrowFlightConfig {
+public:
+  explicit DenodoArrowFlightConfig(
+     std::shared_ptr<const velox::config::ConfigBase> config)
+     : ArrowFlightConfig(config), m_config{config} {}
+
+  static constexpr const char* keyConnectionUsername = "connection.username";
+  static constexpr const char* keyConnectionPassword = "connection.password";
+  static constexpr const char* keyConnectionUserAgent = "connection.user-agent";
+
+  [[nodiscard]] std::optional<std::string> connectionUsername() const;
+  [[nodiscard]] std::optional<std::string> connectionPassword() const;
+  [[nodiscard]] std::optional<std::string> connectionUserAgent() const;
+
+ private:
+  const std::shared_ptr<const velox::config::ConfigBase> m_config;
+};
+
+} // namespace facebook::presto
