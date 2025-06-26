8c216903858350766171b6903c4ca09425325313
Miguel Blanco <mblanco@denodo.com>
2025-06-26 15:51:58 +0000

    Added authentication, does not correctly work yet as the sessionProperties are empty on the authentication side

--- presto-native-execution/presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConnectorFactory.h
+++ presto-native-execution/presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConnectorFactory.h
@@ -1,3 +1,16 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include "presto_cpp/main/connectors/denodo_arrow/DenodoArrowFlightConnector.h"
@@ -17,13 +30,15 @@ public:
   std::shared_ptr<velox::connector::Connector> newConnector(
       const std::string& id,
       std::shared_ptr<const velox::config::ConfigBase> config,
-      folly::Executor* ioExecutor = nullptr,
-      folly::Executor* cpuExecutor = nullptr) override {
+      folly::Executor* ioExecutor,
+      folly::Executor* cpuExecutor) override {
     return std::make_shared<DenodoArrowFlightConnector>(
         id, config, authenticatorName_);
   }
 
-  static std::string keyConnectorName();
+  static constexpr const char* keyConnectorName() {
+    return kConnectorName;
+  }
 
 private:
   static constexpr const char* kConnectorName = "denodo-arrow";
